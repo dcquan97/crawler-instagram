ac91f6d63a8807c3eaa5fc26d3853f3fd893ae39
quan.tomosia <quan.doan@tomosia.com>
2020-03-17 21:21:35 +0700

    done

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,10 +28,18 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: "https://crawler-instagram-website.herokuapp.com/" }
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
-
+  config.active_job.queue_adapter = :sidekiq
+  config.action_mailer.default_url_options = { host: 'https://crawler-instagram-website.herokuapp.com/', port: $PORT }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    user_name: ENV['GMAIL_USERNAME'],
+    password: ENV['GMAIL_PASSWORD'],
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
 
