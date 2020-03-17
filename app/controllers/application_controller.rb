8df409f46489334e5417d52029c23d1d851ce0ee
quan.tomosia <quan.doan@tomosia.com>
2020-03-17 21:32:46 +0700

    done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  helper_method :current_user
+  helper_method :current_user,:remember, :forget, :log_out
   def current_user
     @current_user ||= User.find_by(remember_token: cookies[:remember_token]) if cookies[:remember_token]
   end
