8eb6e68440a1f2a93769dd045760860cce7ca1de
quan.tomosia <quan.doan@tomosia.com>
2020-03-18 10:29:16 +0700

    test crawler

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -39,9 +39,6 @@ class SessionsController < ApplicationController
   end
 
   def crawler
-    if current_user.card_token.blank?
-      redirect_to billing_index_path
-    else
       current_user.update status: false
       CrawlerJob.set(wait: 2.seconds).perform_later(current_user)
       redirect_to dashboard_path
