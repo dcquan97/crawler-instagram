afcf5911288659975d5f0a70ff1fec99a783bd9e
quan.tomosia <quan.doan@tomosia.com>
2020-03-18 13:08:35 +0700

    fix bugs

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,4 +1,4 @@
-Rails.configuration.stripe = {
- :publishable_key => ENV['PUBLISHABLE_KEY'] ||= Rails.application.secrets.stripe_publishable_key,
- :secret_key      => ENV['SECRET_KEY'] ||= Rails.application.secrets.stripe_secret_key
-}
+# Rails.configuration.stripe = {
+#  :publishable_key => ENV['PUBLISHABLE_KEY'] ||= Rails.application.secrets.stripe_publishable_key,
+#  :secret_key      => ENV['SECRET_KEY'] ||= Rails.application.secrets.stripe_secret_key
+# }
